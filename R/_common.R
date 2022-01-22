05879ea74df8180e4383cd06ad7c7bd685e4ad8e
Ben Welman <bwelman@gmail.com>
2022-01-22 17:09:55 +0100

    Opmaak gestroomlijnd met textbook.

--- R/_common.R
+++ R/_common.R
@@ -1,4 +1,4 @@
-set.seed(25)
+#set.seed(25)
 options(digits = 3)
 
 # packages ---------------------------------------------------------------------
@@ -6,24 +6,26 @@ options(digits = 3)
 suppressMessages(library(knitr))
 suppressMessages(library(tidyverse))
 suppressMessages(library(kableExtra))
-suppressMessages(library(patchwork))
 suppressMessages(library(DiagrammeR))
 suppressMessages(library(formattable))
 
 # knitr chunk options ----------------------------------------------------------
 
 knitr::opts_chunk$set(
-  comment = "#>",
-  collapse = TRUE,
-  message = FALSE,
-  warning = FALSE,
-  #cache = TRUE,
-  echo = TRUE, # nodig voor rmd block2 chunks
-  out.width = "60%",
-  fig.align = 'center',
-  fig.width = 6,
-  fig.asp = 0.618,  # 1 / phi
-  fig.show = "hold"
+	echo = FALSE,
+	message = FALSE,
+	warning = FALSE,
+	#cache = TRUE,
+	comment = "#>",
+	collapse = TRUE,
+	fig.retina = 2,       # Control gebruik dpi
+	fig.width = 6,
+	fig.pos = "ht",       # positie figuur latex mode
+	fig.align = 'center',
+	fig.asp = 0.618,      # gulden snede verhouding
+	fig.show = "hold",
+	out.width = "60%",
+	dev.args = list(png = list(type = "cairo-png"))
 )
 
 # knit options -----------------------------------------------------------------
